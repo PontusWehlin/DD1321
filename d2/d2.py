9211238035c2e4e8d59778d02f1ac56831e0af0c
PontusWehlin <pontus168@gmail.com>
2022-01-19 13:31:03 +0100

    create d2

---
+++ d2/d2.py
@@ -0,0 +1,29 @@
+class DictHash:
+    def __init__(self):
+        self.__dict = {}
+
+    def store(self,nyckel,data):
+        self.__dict[nyckel] = data
+
+    def search(self,nyckel):
+        return self.__dict[nyckel]
+
+    def __getitem__(self, nyckel):
+        return self.search(nyckel)
+
+    def __contains__(self, nyckel):
+        return nyckel in self.__dict
+
+def Test():
+    hash = DictHash()
+    hash.store('Ay-money','Mac')
+    hash.store('PP','HP')
+    hash.store('Ameer','MSI')
+
+    print('______TEST_______')
+    print(hash.search('PP'))
+    print(hash['Ay-money'])
+    print('Noak' in hash)
+
+if __name__ == '__main__':
+    Test()
