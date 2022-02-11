8e43178fed81a7ef03c3cad53fd0576d3dbd5654
PontusWehlin <pontus168@gmail.com>
2022-02-11 10:58:22 +0100

    update

--- d2/Hashtabell.py
+++ d2/Hashtabell.py
@@ -1,9 +1,10 @@
 #Global variabel
-List_length = 100000000
+List_length = 10000000
 
 class HashNode:
     def __init__(self):
         self.hash = 1
+        self.index = 1
 
     def __hash__(self, key):
         weight = 1
@@ -24,16 +25,29 @@ class Hashtabell:
         self.dict = [None] * List_length
 
     def __getitem__(self, key):
-        pass
+        index = self.__index()
+        for i in index:
+            if self.dict[i].key == key:
+                return self.dict[i].data
+        raise KeyError('Key not found!')
 
     def __contains__(self, key):
-        pass
+        keys = self.keys()
+        return key in keys
+
+    def __index(self):
+        index = []
+        for nod in self.dict:
+            if nod != None:
+                index.append(nod.index)
+        return index
 
     def store(self,key,data):
         nod = HashNode()
         nod.store(key,data)
         if (self.dict[nod.hash] == None) or (self.dict[nod.hash].key == nod.key):
             self.dict[nod.hash] = nod
+            nod.index = nod.hash
         else:
             probing = True
             index = nod.hash
@@ -42,16 +56,25 @@ class Hashtabell:
                 index *= 2
                 if index > List_length:
                     index = (index % List_length)+1
-                if timeout == 10000:
+                if timeout == 100:
                     probing = False
                     print('Error')
                     print(index)
-
                 if (self.dict[index] == None) or (self.dict[index].key == nod.key):
                     self.dict[index] = nod
-
-                timeout += 2
-
+                    nod.index = index
+                    probing = False
+                timeout += 1
+    def remove(self, key):
+        if self.__contains__(key):
+            index = self.__index()
+            for i in index:
+                if self.dict[i].key == key:
+                    data = self.dict[i].data
+                    self.dict[i] = None
+        else:
+            raise KeyError('Key not found!')
+        return data
 
     def keys(self):
         keys=[]
@@ -60,10 +83,11 @@ class Hashtabell:
                 keys.append(nod.key)
         return keys
 
+
 def import_tracks(filename, hashdict):
     file = open(filename,"r",encoding="UTF-8")
     file_lines = file.readlines()
-    for line in file_lines[:200]:
+    for line in file_lines:
         line_parts = line.split("<SEP>")
         hashdict.store(line_parts[-2], line_parts[-1].rstrip("\n"))
     return hashdict
@@ -72,8 +96,15 @@ def import_tracks(filename, hashdict):
 def main():
     hashdict = Hashtabell()
     hashdict = import_tracks("unique_tracks.txt", hashdict)
+    print('Imported')
     print(len(hashdict.keys()))
-    print(hashdict.keys()[:50])
+    print("U2" in hashdict)
+    print(hashdict['U2'])
+    print(hashdict.remove('U2'))
+    print('U2' in hashdict)
+
+
+
 
 
 
