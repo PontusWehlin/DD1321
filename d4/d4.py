31b73d25e1502821eeed0347d80550b628ee3368
PontusWehlin <pontus168@gmail.com>
2022-02-21 15:29:09 +0100

    Update d4.py

--- d4/d4.py
+++ d4/d4.py
@@ -9,7 +9,7 @@ class Song:
         self.songid = songid
 
     def __lt__(self, other):
-        return self.artist < other
+        return self.artist < other.artist
 
 def readfile(filename):
     songlist = []
@@ -29,6 +29,42 @@ def linsearch(list, testartist):
             return list[i]
 
 
+##### Taget från föreläsnings anteckningar
+def quicksort(data):
+    sista = len(data) - 1
+    qsort(data, 0, sista)
+
+def qsort(data, low, high):
+    pivotindex = (low + high) // 2
+    # flytta pivot till kanten
+    data[pivotindex], data[high] = data[high], data[pivotindex]
+
+    # damerna först med avseende på pivotdata
+    pivotmid = partitionera(data, low - 1, high, data[high])
+
+    # flytta tillbaka pivot
+    data[pivotmid], data[high] = data[high], data[pivotmid]
+
+    if pivotmid - low > 1:
+        qsort(data, low, pivotmid - 1)
+    if high - pivotmid > 1:
+        qsort(data, pivotmid + 1, high)
+
+def partitionera(data, v, h, pivot):
+    while True:
+        v = v + 1
+        while data[v] < pivot:
+            v = v + 1
+        h = h - 1
+        while h != 0 and data[h] > pivot:
+            h = h - 1
+        data[v], data[h] = data[h], data[v]
+        if v >= h:
+            break
+    data[v], data[h] = data[h], data[v]
+    return v
+
+
 def main():
     filename = "unique_tracks.txt"
     # file_del2 = "sang-artist-data.txt"
@@ -42,6 +78,10 @@ def main():
 
     linjtid = timeit.timeit(stmt=lambda: linsearch(lista, testartist), number=100)
     print("Linjärsökningen tog", round(linjtid, 4), "sekunder")
+
+    sort_list = quicksort(lista)
+    linjsorttid = timeit.timeit(stmt=lambda: linsearch(sort_list, testartist), number=100)
+    print('Linjärsökning för sorterad lista tog', round(linjsorttid, 4), 'sekunder')
 
 
 if __name__ == '__main__':
